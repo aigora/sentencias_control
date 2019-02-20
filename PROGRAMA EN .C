a8b5d08a890b9479cd7a1643b66fa303cb9b51ce
SamuelHornsbyAltares <47475011+SamuelHornsbyAltares@users.noreply.github.com>
2019-02-20 13:14:49 +0100

    Matrículas de coches parking

---
+++ PROGRAMA EN .C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	int matricula,distintivo,C,B,A,impar;
+	printf("Introduzca los numeros de la matricula de su vehiculo\t");
+	scanf("%d",&matricula);
+	impar=matricula%2;
+	
+	if(matricula%2==0) {
+		printf("No tiene acceso al estacionamiento en el casco antiguo de la ciudad\n");
+	}
+		if(impar) {
+		printf("Indique su distintivo de contaminacion:\t");
+		printf("A\t");
+		printf("B\t");
+		printf("C\t");
+		scanf("%d",&distintivo);	
+		}
+			if(distintivo==C) {
+				printf("Usted puede aparcar solo en la zona 3\n\a");
+			}
+			else if(distintivo==B) {
+				printf("Usted puede aparcar solo en las zonas 2 y 3\n\a\a");
+			}
+			else {
+				printf("Usted puede aparcar en las zonas 1, 2 y 3\n\a\a\a");
+			}
+system("PAUSE");
+return 0;
+}
