f63e78deb34d1cbf5c51eb6a40ca274eea007a16
mrodrigog <37368736+mrodrigog@users.noreply.github.com>
2019-02-20 13:20:43 +0100

    numero mayor o menor que 50

---
+++ entrega.c
@@ -0,0 +1,39 @@
+
+
+//miguel angel rodrigo garcia
+//q103
+//saber si el numero introducido es mayor o menor que 50
+
+#include<stdio.h>
+
+
+#include<math.h>
+int main()
+{
+	int n;
+	//escribimos un bucle para saber si el numero
+	// es mayor menor o igual de 50
+	printf("escriba un numero\n");
+	scanf("%d",&n);
+	if( n<50)
+	
+	
+	printf("el numero es menor que 50\n");
+      
+	else if(n==50)
+	
+	
+	printf("el numero es 50\n");
+        
+	else 
+	
+	
+	
+	
+	printf("el numero es mayor de 50\n");
+}
+	
+     
+	
+
+
