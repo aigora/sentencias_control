2f38f4f1442ace74e4a0885a17cd4f896629f076
hananelboud <47193419+hananelboud@users.noreply.github.com>
2019-02-20 13:25:00 +0100

    Introduce una nota y te digo si has aprobado

---
+++ ValoracionDeNota.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	float nota;
+	printf("Introduzca la nota de su examen del uno al diez con dos decimales:\n");
+	scanf("%.2f",&nota);
+	if (nota<0){
+		printf("Introduzca una nota correcta xfa ");
+	}
+	else if(nota < 5){
+		printf("Lo siento has suspendido");
+	}
+	else if (5>=nota&&nota<=9){
+		printf("Enhorabuena has aprobado\n");
+	}
+	else if(nota==10){
+		printf("Has aprobado. Ademas tienes Matricula de Honor");
+		}
+	
+	else if (nota>10){
+		printf("Introduzca una nota correcta xfa ");
+	}
+	return 0;
+}
