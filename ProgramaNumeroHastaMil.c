1cadae02ce7a17a595cc382f24d24597669181de
andreeea6 <47365611+andreeea6@users.noreply.github.com>
2019-02-24 21:06:22 +0100

    programa imprime numero del uno al mil
    (sentencias condicionales y bucle for)

---
+++ ProgramaNumeroHastaMil.c
@@ -0,0 +1,14 @@
+/*
+Autora: Andrea Fernández Ortiz 
+Grupo: Q103
+Descripción: programa que te los numeros del 1 al 1000
+*/
+
+#include<stdio.h>
+void main(){
+	int i;
+	
+	for(i=1; i<=1000; i++){
+		printf("%d\n",i);
+	}
+}
