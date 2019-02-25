f163eae3f8a61f712043486698f99ddef142147e
mariacanga <47222561+mariacanga@users.noreply.github.com>
2019-02-25 10:13:09 +0100

    Bucle for: Numeros Impares o Pares del 1 al 50

---
+++ NumerosParesImpares.c
@@ -0,0 +1,30 @@
+/*
+Autora: María Canga
+Grupo: Q-103
+Descripción: Números pares o impares del 1 al 50
+*/
+
+#include <stdio.h>
+
+int main() {
+	int i, num, num1, cont=0;
+	
+	printf ("1- Numeros impares / 2- Numeros pares\n");
+	scanf ("%i", &num);
+	
+	if (num==1){
+		printf ("Numeros impares del 1 al 50:\n");
+	    for (i=1; i<=50; i+=2){
+		    printf ("%i ", i);
+	    }
+	}
+	else if (num==2){
+		printf ("Nueros pares del 1 al 50:\n");
+		for (i=2; i<=50; i+=2){
+		    printf ("%i ", i);
+	    }
+	}
+	else {
+		printf ("Opcion incorrecta.");
+	}
+}
