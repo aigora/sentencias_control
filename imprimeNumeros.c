763cc171c2b664990e25701bb404c8ceb92c7728
DalilaMB <37144372+DalilaMB@users.noreply.github.com>
2019-02-25 10:05:53 +0100

    Imprime lista de numeros

---
+++ imprimeNumeros.c
@@ -0,0 +1,12 @@
+//Autora: Dalila Martinez Baez Q103
+//Imprime una lista desde el 0 hasta el 20
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=0;i<=20;i++)
+	{
+		printf("%i \n",i);
+	}
+	return 0;
+}
