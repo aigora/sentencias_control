b7c9d7f62c5c65a66765689f9cdc3928d3bbb6b9
Carri0202 <47537854+Carri0202@users.noreply.github.com>
2019-02-25 10:24:28 +0100

    Números impares del 1 al 100

---
+++ Numerosimparesbuclesfor.c
@@ -0,0 +1,25 @@
+/*David Carrillo Rodríguez
+Q103
+Números impares*/
+
+#include <stdio.h>
+
+
+int main()
+{
+            int x;  
+           
+            for (x=1;x<=100;x++)
+            {
+        if (x%2!=0)
+        {
+           printf("%d\n",x);
+        }
+    }
+           
+    system("PAUSE");     
+    return 0;
+}
+
+
+
