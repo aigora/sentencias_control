7a72b40f62e89b6e3d44448dd8026352120d9d87
FernandoCasillas <47816021+FernandoCasillas@users.noreply.github.com>
2019-02-25 10:26:57 +0100

    ejercicio BucleFor numeros

---
+++ numerosbucle15.c
@@ -0,0 +1,20 @@
+/*
+Nombre: Fernando Casillas Juan
+Grupo Q103
+Descripcion: Numero del 1 al 15.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int contador; 
+    for( contador=1; contador<16 ; contador=contador+1) 
+	{
+    
+    printf("\n%d",contador);
+    
+    }
+    
+    return 0;
+}
