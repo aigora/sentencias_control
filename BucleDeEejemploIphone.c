137850b374d61b345d0fcb5c1cb56405c278caa8
javierlopez1 <47518821+javierlopez1@users.noreply.github.com>
2019-02-25 10:00:58 +0100

    Ejemplo de bucles con contador

---
+++ BucleDeEejemploIphone.c
@@ -0,0 +1,14 @@
+/* Autor: javier lopez fernandez
+grupo: Q103
+descripcion: ejemplo basico de un bulce que muestra una sucesion de numeros */
+
+
+#include <stdio.h>
+
+int main() {
+int i ;
+for (i=0; i<10^10;i++) {
+	printf(" los Iphone son basura %i \n",i);
+}
+}
+
