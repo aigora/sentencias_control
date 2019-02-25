a3291e5d7d52c7139d45fde5bddd1f2c460aae3b
8alvarogarcia <47796573+8alvarogarcia@users.noreply.github.com>
2019-02-25 10:23:31 +0100

    Add files via upload
    Bucle que imprime los cuadrados de los números del 1 al 50

---
+++ bucle_alvarogarcia.c
@@ -0,0 +1,15 @@
+//Alvaro Garcia 54611
+//Grupo Q103
+//Bucle for
+
+#include <stdio.h>
+int main()
+{
+	int n,a;
+		for(n=1;n<=50;n++)
+			{
+				a=n*n;
+				printf("%i al cuadrado= %i\n",n,a);
+			}
+}
+
