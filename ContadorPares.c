bbe2156c650aa71a29905b9ab0b81d3edb9f64b8
lauraubeda <47366968+lauraubeda@users.noreply.github.com>
2019-02-25 10:26:06 +0100

    Programa con bucle for
    Contador de números pares

---
+++ ContadorPares.c
@@ -0,0 +1,13 @@
+/* Autora: Laura Úbeda
+Grupo: Q103 
+Descripción: Este programa imprime un contador de números pares hasta el 20*/
+
+#include <stdio.h>
+int main() {
+int x;
+for (x =2; x<=20; x+=2)
+{
+	printf("El contador vale: %i\n", x);
+}
+return 0;
+}
