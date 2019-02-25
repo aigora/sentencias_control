2ffa4236e166ab74e4ce3d7b29fec5b2887661d1
Sandraahg <47281035+Sandraahg@users.noreply.github.com>
2019-02-25 10:12:00 +0100

    Bucle_for_50_veces
    El programa repite 50 veces: Fumar perjudica la salud

---
+++ Bucle for.c
@@ -0,0 +1,13 @@
+/* Autora: Sandra Hueros Garcia
+Grupo Q-103
+Descripcion: Repite 50 veces que fumar perjudica la salud*/
+
+#include <stdio.h>
+
+int main (){
+	int i;
+	
+	for (i=1;i<=50;i++){
+		printf("%i. Fumar perjudica la salud. \n", i); 
+	}
+}
