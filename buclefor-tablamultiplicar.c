d39746f96a4ba1ddf7b99b444ba4873ebbf1b513
marinarx20 <47271160+marinarx20@users.noreply.github.com>
2019-02-25 09:55:41 +0100

    La tabla de multiplicar del 1

---
+++ buclefor-tablamultiplicar.c
@@ -0,0 +1,13 @@
+/*Autora: Marina Rodríguez Martinez
+Grupo: Q-103
+Descripcion: La tabla de multiplicar del 1*/
+
+
+#include<stdio.h>
+int main(){
+	int i, numero;	
+	printf("La tabla de multiplicar del 1", numero);
+
+	for ( i=1; i<=10; i++)
+	 printf("\n %d*%d=%d", numero,i, numero*i);
+}
