bf8fc2966ed2f0a3b210d8cdb9957156735d4d2b
andreeeaaa <47831640+andreeeaaa@users.noreply.github.com>
2019-02-25 10:00:17 +0100

    Te repite la palabra Hola 11 veces

---
+++ BucleHola.c
@@ -0,0 +1,21 @@
+/*ANDREA BLAS MURILLO 
+Q-103 
+DEscripción: Bucle for que te dice once veces hola*/
+
+
+
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+for(i=10;i>=0;i--)
+{
+
+printf("Hola ");
+
+}
+}
+
+
+
