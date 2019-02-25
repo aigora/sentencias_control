f5e95515e04618b6ceace57d67803d8416b76dc9
MrHook6 <47815619+MrHook6@users.noreply.github.com>
2019-02-25 09:57:15 +0100

    Tablas de multiplicar

---
+++ tablas_de_multiplicar.c
@@ -0,0 +1,29 @@
+/*
+ Diego Sena
+Q103
+Tablas de multiplicar
+*/
+
+
+
+
+#include <stdio.h>
+
+
+int main()
+{
+            int x,y;
+            
+            for (x=0;x<=10;x++)
+            {
+        for (y=1;y<=10;y++)
+                {
+            printf("%d X %d = %d \n",x,y,x*y);
+        }
+        printf("\n");
+    }
+            printf("\n");
+            
+         
+    return 0;
+}
