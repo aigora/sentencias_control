85f53cddf3d532fb9277cbc476557ea16dc203d2
pauladepaz <37063029+pauladepaz@users.noreply.github.com>
2019-02-26 18:23:16 +0100

    ¿Qué escribe el siguiente programa?

---
+++ ifelseif.c
@@ -0,0 +1,21 @@
+//Paula de Paz Gómez
+//Numero matricula: 53917
+//Ejemplo del uso de if-else, se trata de razonar antes de compilar, la letra que imprimiría en pantalla el siguiente programa.
+
+#include <stdio.h>
+
+int main ()
+{
+	int i = 1;
+	if (i<0)
+	printf("A");
+	else if (i>15)
+	printf("B");
+	else if (i==5)
+	printf("c");
+	else if (i<5)
+	printf("D");
+	else 
+	printf("E");
+
+}
