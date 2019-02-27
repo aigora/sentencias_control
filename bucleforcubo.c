b4f7dd69f19ea6c322049bed8a18af5ea64b7fb9
mariogarridomontero <47970764+mariogarridomontero@users.noreply.github.com>
2019-02-27 12:27:01 +0100

    bucle que permite calcular el cubo de los numeros del 1 al 100

---
+++ bucleforcubo.c
@@ -0,0 +1,20 @@
+
+
+// Mario Garrido 
+//Q103
+//bucle for indicando los numeros ordenados del 1 al 100 y ellos mismos elevados al cubo.
+#include <stdio.h>
+int main(){
+	int i,z;
+	for(i=1;i<=100;i++){ //numeros del 1 al 100 elevados al cubo
+		z=i*i*i;
+		printf("%i al cubo= %i\n",i,z);
+	}
+	
+	
+	
+	return 0;
+	
+}
+
+
