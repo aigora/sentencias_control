3d3ac0e87ab8c76bf54ec40d9cecaa2314fa3437
Hugo García Las Heras <38069816+huguiigar@users.noreply.github.com>
2019-02-27 17:10:26 +0100

    Múltiplos de de 7 entre un intervalo del usuario

---
+++ buclecondicionentrenumeros.c
@@ -0,0 +1,21 @@
+/* Autor:Hugo García Las Heras
+Grupo:Q-203
+Descripción: El usuario introduce dos numeros enteros y el ordenador imprime por pantalla los numeros entre ellos dos que sean múltiplos de 7 siempre y cuando el segundo número introducido sea mayor 
+que el primero y que ambos sean mayores de cero */
+#include<stdio.h>
+void main()
+{
+	int x , y , i; 
+    do 
+	{
+	printf("Introduce el primer numero:\n");
+	scanf("%d",&x);
+	printf("Introduce el segundo numero:\n");
+	scanf("%d",&y);
+	}
+	while((x>=y)||(x<=0)||(y<=0));
+	 printf("Los multiplos de 7 entre %d y %d son:",x,y);
+	for(i=x;i<=y;i++)
+	    if(i % 7==0)
+		printf("%d\n",i);
+    }
