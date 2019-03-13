3069ee9a0c2ec4fe898a50195ad29a69085806e1
gmorenod <37963888+gmorenod@users.noreply.github.com>
2019-03-13 11:14:00 +0100

    Ejercicio 9 Tema 3(bucles)

---
+++ t3bucles_e9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+	 int n;
+	 for(n=1;n<=10;n++){
+	 	printf("%d\n",n);
+	 	if(n==3)
+	 	printf("\n He llegado al numero 3\n");
+	 	if(n==10)
+	 	printf("\n He acabado de contar\n");
+	 }
+	 
+}
